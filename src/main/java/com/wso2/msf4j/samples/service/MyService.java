50f04b7b69e423148bf96584420c2ccee9ec605e
Edgar Silva <edgarsilva@gmail.com>
2016-02-22 00:16:43 -0300

    Update MyService.java

--- src/main/java/com/wso2/msf4j/samples/service/MyService.java
+++ src/main/java/com/wso2/msf4j/samples/service/MyService.java
@@ -13,6 +13,21 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/*
+ * Copyright (c) 2016, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.wso2.msf4j.samples.service;
 
@@ -31,6 +46,7 @@ import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -42,8 +58,11 @@ import org.milyn.container.ExecutionContext;
 import org.milyn.event.report.HtmlReportGenerator;
 import org.milyn.io.StreamUtils;
 import org.milyn.payload.StringResult;
+import org.wso2.msf4j.internal.router.beanconversion.JsonConverter;
 import org.wso2.msf4j.template.MustacheTemplateEngine;
 import org.xml.sax.SAXException;
+
+import com.google.gson.JsonParser;
 
 /**
  * This is the Microservice resource class.
@@ -135,9 +154,13 @@ public class MyService {
     
     @GET
     @Path("/smooks") 
-    public String postConvertionFromSmooks( @QueryParam("edi") String edi)  {
+
+    public Response postConvertionFromSmooks( @QueryParam("edi") String edi)  {
     	
-    	return new EDIFactParser().parse(edi);
+       String response = new EDIFactParser().parse(edi);
+    	
+       return Response.status(Response.Status.OK).entity(response).build();
+    	
     	
     }
     
